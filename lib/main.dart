eb46505fb48d95f0ecf7e619e7565fa7a406a1a2
Taofik <taofik@yaraa.io>
2023-05-27 01:43:49 +0100

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/main_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
 
 void main() {
@@ -11,14 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const WelcomePage(),
+      home: MainPage(),
     );
   }
 }
---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/bar_item_page.dart';
+import 'package:travel_app/pages/navpages/home_page.dart';
+import 'package:travel_app/pages/navpages/my_page.dart';
+import 'package:travel_app/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = const [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage(),
+  ];
+
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BottomNavigationBar(
+            unselectedFontSize: 0,
+            selectedFontSize: 0,
+            onTap: onTap,
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.white,
+            currentIndex: currentIndex,
+            selectedItemColor: Colors.black54,
+            unselectedItemColor: Colors.grey.withOpacity(0.5),
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            elevation: 0,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.apps), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.bar_chart_sharp), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Home'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
